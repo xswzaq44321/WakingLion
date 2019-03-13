cb8cfda5760415c9d70335eacf9952a243f94fc8
Weber <xswzaq44321@gmail.com>
2019-03-13 18:06:40 +0800

    add ex3

--- Operator_overloading_and_template/ex3.cpp
+++ Operator_overloading_and_template/ex3.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
+T getMax(T a, T b){
+	return (a > b ? a : b);
+}
+
+int getMax(int a, int b){
+	cout << "int";
+	return (a > b ? a : b);
+}
+
 
 int main(){
+	cout << getMax(2, 3) << endl;
+	cout << getMax(3.2, 9.4) << endl;
+	cout << getMax(5, 3) << endl;
+	cout << getMax((double)-20, 22.2) << endl;
+	cout << getMax('a', 'z') << endl;
 }
